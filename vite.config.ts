ef89ecde60cb6f3b4dde508bd025b182e9374fe0
Sidhheshdongare <sidhheshdesigner@gmail.com>
2025-05-11 13:38:50 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 });
-import { ScrollSmoother } from "gsap/ScrollSmoother";
 gsap.registerPlugin(ScrollSmoother);
