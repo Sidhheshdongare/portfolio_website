0b60073ff2e247c3d6be3734954b8f977ea2f8a8
Sidhheshdongare <sidhheshdesigner@gmail.com>
2025-05-11 15:06:35 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,6 @@
+import { useLayoutEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
